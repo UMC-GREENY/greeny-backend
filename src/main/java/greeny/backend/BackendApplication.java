a0dbdd3ff23166ba92c7efbcc55fa4414edc3a0d
Minuooooo <mmw7741@gmail.com>
2023-07-16 23:41:32 +0900

    [ env ] Divide common, dev, prod yml

--- src/main/java/greeny/backend/BackendApplication.java
+++ src/main/java/greeny/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package greeny.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:secure.properties")
 public class BackendApplication {
 
 	public static void main(String[] args) {
