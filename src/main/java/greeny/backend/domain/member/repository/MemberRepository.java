be6932f7532a27c00d330c8fab91d4f37269831d
Minuooooo <mmw7741@gmail.com>
2023-07-17 17:21:16 +0900

    [ feat ] Member repository

---
+++ src/main/java/greeny/backend/domain/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package greeny.backend.domain.member.repository;
+
+import greeny.backend.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
