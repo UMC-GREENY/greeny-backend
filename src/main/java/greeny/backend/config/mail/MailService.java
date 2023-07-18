fd713f749a265dc2600daaeb35945ba14eedf3e3
Minuooooo <mmw7741@gmail.com>
2023-07-18 15:44:34 +0900

    [ feat ] Config mail

---
+++ src/main/java/greeny/backend/config/mail/MailService.java
@@ -0,0 +1,85 @@
+package greeny.backend.config.mail;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+    private final String code = generateCode();
+
+    public String sendSimpleMessage(String to) throws MessagingException, UnsupportedEncodingException {
+
+        MimeMessage message = createMessage(to);
+
+        try {
+            javaMailSender.send(message);
+        } catch (MailException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+
+        return code;
+    }
+
+    private String generateCode() {
+        StringBuilder code = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < 8; i++) {
+
+            int index = random.nextInt(3);
+
+            switch (index) {
+                case 0:
+                    code.append((char) (random.nextInt(26) + 97));
+                    break;
+                case 1:
+                    code.append((char) (random.nextInt(26) + 65));
+                    break;
+                case 2:
+                    code.append((random.nextInt(10)));
+                    break;
+            }
+        }
+
+        return code.toString();
+    }
+
+    private MimeMessage createMessage(String to) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.addRecipients(Message.RecipientType.TO, to);
+        message.setSubject("GREENY 이메일 인증");
+
+        String msgg="";
+        msgg+= "<div style='margin:20px;'>";
+        msgg+= "<h1> 안녕하세요 GREENY 입니다. </h1>";
+        msgg+= "<br>";
+        msgg+= "<p>아래 코드를 복사해 입력해주세요<p>";
+        msgg+= "<br>";
+        msgg+= "<p>감사합니다.<p>";
+        msgg+= "<br>";
+        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
+        msgg+= "<h3 style='color:blue;'>이메일 인증 코드입니다.</h3>";
+        msgg+= "<div style='font-size:130%'>";
+        msgg+= "CODE : <strong>";
+        msgg+= code+"</strong><div><br/> ";
+        msgg+= "</div>";
+        message.setText(msgg, "utf-8", "html");
+        message.setFrom(new InternetAddress("mmw7741@gmail.com","GREENY"));
+
+        return message;
+    }
+}
