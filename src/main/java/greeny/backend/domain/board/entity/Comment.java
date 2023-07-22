c1260ee01e7fc11b90faf8d78c4637562668ff5a
June <dtd1614@gmail.com>
2023-07-22 20:04:49 +0900

    [ feat ] Comment entity

---
+++ src/main/java/greeny/backend/domain/board/entity/Comment.java
@@ -0,0 +1,27 @@
+package greeny.backend.domain.board.entity;
+
+import greeny.backend.domain.AuditEntity;
+import greeny.backend.domain.member.entity.Member;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Builder
+public class Comment extends AuditEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id", nullable = false)
+    private Member writer;
+    @Column(length = 500, nullable = false)
+    private String content;
+}
