b83d0e883db0efbbc1d4d168fb73a7c30fcf2106
June <dtd1614@gmail.com>
2023-07-25 23:49:54 +0900

    [ feat ] Content search

--- src/main/java/greeny/backend/domain/board/repository/PostRepository.java
+++ src/main/java/greeny/backend/domain/board/repository/PostRepository.java
@@ -12,7 +12,8 @@ import java.util.Optional;
 public interface PostRepository extends JpaRepository<Post, Long> {
     @NotNull
     Page<Post> findAll(@NotNull Pageable pageable);
-    Page<Post> findAllByTitleContaining(String keyword, Pageable pageable);
+    //제목 검색 -> 제목+내용 검색. IgnoreCase : 대소문자 구별 무시
+    Page<Post> findAllByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(String title, String content, Pageable pageable);
     @EntityGraph(attributePaths = {"postFiles"})
     @NotNull
     Optional<Post> findById(@NotNull Long id);
