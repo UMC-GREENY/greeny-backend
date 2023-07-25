ef351c5beb378bebbf854c2d43522c6ecf4db132
June <dtd1614@gmail.com>
2023-07-25 23:52:49 +0900

    [ feat ] Check post exist

--- src/main/java/greeny/backend/domain/board/repository/PostRepository.java
+++ src/main/java/greeny/backend/domain/board/repository/PostRepository.java
@@ -14,7 +14,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAll(@NotNull Pageable pageable);
     //제목 검색 -> 제목+내용 검색. IgnoreCase : 대소문자 구별 무시
     Page<Post> findAllByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(String title, String content, Pageable pageable);
-    @EntityGraph(attributePaths = {"postFiles"})
     @NotNull
     Optional<Post> findById(@NotNull Long id);
     Page<Post> findAllByWriterId(Long writerId, Pageable pageable);
