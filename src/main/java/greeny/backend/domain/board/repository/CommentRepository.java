51c2bf3d9467bf70c25faf76f2b167e309960883
June <dtd1614@gmail.com>
2023-07-28 23:23:51 +0900

    [ feat ] Fetch join

--- src/main/java/greeny/backend/domain/board/repository/CommentRepository.java
+++ src/main/java/greeny/backend/domain/board/repository/CommentRepository.java
@@ -1,10 +1,13 @@
 package greeny.backend.domain.board.repository;
 
 import greeny.backend.domain.board.entity.Comment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @EntityGraph(attributePaths = {"writer"}) // writer를 fetch join으로 한번에 조회
     List<Comment> findAllByPostId(Long postId);
 }
--- src/main/java/greeny/backend/domain/board/repository/PostRepository.java
+++ src/main/java/greeny/backend/domain/board/repository/PostRepository.java
@@ -10,11 +10,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @NotNull
-    Page<Post> findAll(@NotNull Pageable pageable);
+    @EntityGraph(attributePaths = {"writer", "postFiles"}) // writer와 postFiles을 fetch join으로 한번에 조회
+    @NotNull Page<Post> findAll(@NotNull Pageable pageable);
+
     //제목 검색 -> 제목+내용 검색. IgnoreCase : 대소문자 구별 무시
+    @EntityGraph(attributePaths = {"writer", "postFiles"})
     Page<Post> findAllByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(String title, String content, Pageable pageable);
-    @NotNull
-    Optional<Post> findById(@NotNull Long id);
+
+    @EntityGraph(attributePaths = {"writer", "postFiles"})
+    @NotNull Optional<Post> findById(@NotNull Long id);
+
+    @EntityGraph(attributePaths = {"writer", "postFiles"})
     Page<Post> findAllByWriterId(Long writerId, Pageable pageable);
 }
