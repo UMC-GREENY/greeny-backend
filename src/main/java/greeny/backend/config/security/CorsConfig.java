2dd3327098dfd79b303cc2cd1c79fcf988273aae
Minuooooo <mmw7741@gmail.com>
2023-07-31 09:43:21 +0900

    [ feat ] Allow origin

--- src/main/java/greeny/backend/config/security/CorsConfig.java
+++ src/main/java/greeny/backend/config/security/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");  // TODO 3000번 포트, EC2 & Front 도메인 주소
+        config.addAllowedOrigin("http://www.greeny.r-e.kr");  // Server base url
+        config.addAllowedOrigin("http://localhost:3000");  // 프론트 개발 환경 (3000번 포트)
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
