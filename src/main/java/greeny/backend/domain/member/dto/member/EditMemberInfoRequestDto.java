050fc468c550fb8431f67dd011dd60abc11e0490
Minuooooo <mmw7741@gmail.com>
2023-08-03 14:38:22 +0900

    [ fix ] Change regualr expression

--- src/main/java/greeny/backend/domain/member/dto/member/EditMemberInfoRequestDto.java
+++ src/main/java/greeny/backend/domain/member/dto/member/EditMemberInfoRequestDto.java
@@ -20,7 +20,7 @@ public class EditMemberInfoRequestDto {
 
     //회원이 바꾸려는 비밀번호를 입력받습니다.
     @NotBlank(message = "새로운 비밀번호를 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z0-9])(?=.*[!@#$%^&*]).{8,}$", message = "비밀번호는 영숫자, 특수문자가 필수이고 8자리 이상이어야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z0-9])(?=.*[!@#$%^&*]).{8,15}$", message = "비밀번호는 영숫자, 특수문자가 필수이고 8자리 이상 15이내여야 합니다.")
     @Schema(description = "새로운 비밀번호", defaultValue = "test5678@")
     private String passwordToChange;
 }
