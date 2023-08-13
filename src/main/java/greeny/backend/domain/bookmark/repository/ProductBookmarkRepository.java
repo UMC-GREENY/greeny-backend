d4e5554e6bd672de175fd9a1499ba8ad6d10a2db
Hyun Seong Kim <122839143+evgeniac10@users.noreply.github.com>
2023-08-13 15:29:58 +0900

    [feat] 북마크 기능 추가 (#37)

--- src/main/java/greeny/backend/domain/bookmark/repository/ProductBookmarkRepository.java
+++ src/main/java/greeny/backend/domain/bookmark/repository/ProductBookmarkRepository.java
@@ -1,7 +1,10 @@
 package greeny.backend.domain.bookmark.repository;
 
 import greeny.backend.domain.bookmark.entity.ProductBookmark;
+import greeny.backend.domain.bookmark.entity.StoreBookmark;
 import greeny.backend.domain.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -18,4 +21,7 @@ public interface ProductBookmarkRepository extends JpaRepository<ProductBookmark
     @EntityGraph(attributePaths = {"product"})
     List<ProductBookmark> findProductBookmarksByLiker(Member liker);
     Optional<ProductBookmark> findByProductIdAndLikerId(Long productId, Long likerId);
+
+    @EntityGraph(attributePaths = {"product"})
+    Page<ProductBookmark> findProductBookmarksByLiker(Pageable pageable , Member liker); //Member에 대한 Product bookmark 리스트 가져오기
 }
