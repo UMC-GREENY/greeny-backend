c29708072078e97e3ca2ea3bfcb1f1291bdd0acf
Minuooooo <mmw7741@gmail.com>
2023-08-23 22:21:14 +0900

    [ feat ] Get simple review infos with type & idByType

--- src/main/java/greeny/backend/domain/review/dto/GetReviewListResponseDto.java
+++ src/main/java/greeny/backend/domain/review/dto/GetReviewListResponseDto.java
@@ -18,8 +18,10 @@ public class GetReviewListResponseDto {
     private int star;
     private String content;
     private boolean existsFile;
+    private String type;
+    private Long idByType;
 
-    public static GetReviewListResponseDto toStoreReviewDTO(StoreReview review) {
+    public static GetReviewListResponseDto from(StoreReview review) {
         return GetReviewListResponseDto.builder()
                 .id(review.getId())
                 .createdAt(review.getCreatedAt())
@@ -29,7 +31,20 @@ public class GetReviewListResponseDto {
                 .existsFile(!review.getStoreReviewImages().isEmpty())
                 .build();
     }
-    public static GetReviewListResponseDto toProductReviewDTO(ProductReview review) {
+    public static GetReviewListResponseDto toDetailStoreDto(StoreReview review, String type, Long idByType) {
+        return GetReviewListResponseDto.builder()
+                .id(review.getId())
+                .createdAt(review.getCreatedAt())
+                .writerEmail(review.getReviewer().getEmail())
+                .star(review.getStar())
+                .content(review.getContent())
+                .existsFile(!review.getStoreReviewImages().isEmpty())
+                .type(type)
+                .idByType(idByType)
+                .build();
+    }
+
+    public static GetReviewListResponseDto from(ProductReview review) {
         return GetReviewListResponseDto.builder()
                 .id(review.getId())
                 .createdAt(review.getCreatedAt())
@@ -39,5 +54,16 @@ public class GetReviewListResponseDto {
                 .existsFile(!review.getProductReviewImages().isEmpty())
                 .build();
     }
-
+    public static GetReviewListResponseDto toDetailProductDto(ProductReview review, String type, Long idByType) {
+        return GetReviewListResponseDto.builder()
+                .id(review.getId())
+                .createdAt(review.getCreatedAt())
+                .writerEmail(review.getReviewer().getEmail())
+                .star(review.getStar())
+                .content(review.getContent())
+                .existsFile(!review.getProductReviewImages().isEmpty())
+                .type(type)
+                .idByType(idByType)
+                .build();
+    }
 }
--- src/main/java/greeny/backend/domain/review/service/ReviewService.java
+++ src/main/java/greeny/backend/domain/review/service/ReviewService.java
@@ -31,9 +31,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.ArrayList;
 import java.util.List;
 
-import static greeny.backend.domain.review.dto.GetReviewListResponseDto.toProductReviewDTO;
-import static greeny.backend.domain.review.dto.GetReviewListResponseDto.toStoreReviewDTO;
-
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
@@ -50,17 +47,18 @@ public class ReviewService {
     public void writeStoreReview(Long id, WriteReviewRequestDto writeReviewRequestDto, List<MultipartFile> multipartFiles, Member member) {
         //storeReview 저장
         Store store = storeRepository.findById(id).orElseThrow(StoreNotFound::new);
-        StoreReview storeReview =storeReviewRepository.save(writeReviewRequestDto.toStoreReviewEntity(member, store));
-        if(multipartFiles!=null){
-            uploadFiles(multipartFiles,storeReview);
+        StoreReview storeReview = storeReviewRepository.save(writeReviewRequestDto.toStoreReviewEntity(member, store));
+        if (multipartFiles != null) {
+            uploadFiles(multipartFiles, storeReview);
         }
     }
+
     @Transactional
-    public void writeProductReview(Long id, WriteReviewRequestDto writeReviewRequestDto,List<MultipartFile> multipartFiles,Member member) {
+    public void writeProductReview(Long id, WriteReviewRequestDto writeReviewRequestDto, List<MultipartFile> multipartFiles, Member member) {
         Product product = productRepository.findById(id).orElseThrow(ProductNotFound::new);
-        ProductReview productReview = productReviewRepository.save(writeReviewRequestDto.toProductReviewEntity(member,product));
-        if(multipartFiles!=null){
-            uploadFiles(multipartFiles,productReview);
+        ProductReview productReview = productReviewRepository.save(writeReviewRequestDto.toProductReviewEntity(member, product));
+        if (multipartFiles != null) {
+            uploadFiles(multipartFiles, productReview);
         }
     }
 
@@ -68,53 +66,79 @@ public class ReviewService {
     /* 내가 쓴 리뷰 불러오기 */
     @Transactional(readOnly = true)
     public Page<GetReviewListResponseDto> getMemberReviewList(String type, Pageable pageable, Member member) {
-        if(type.equals("store")) {
-            Page<StoreReview> pages = storeReviewRepository.findAllByReviewer(pageable,member);
-            return pages.map(GetReviewListResponseDto::toStoreReviewDTO);
-        } else if(type.equals("product")) {
-            Page<ProductReview> pages = productReviewRepository.findAllByReviewer(pageable,member);
-            return pages.map(GetReviewListResponseDto::toProductReviewDTO);
-        } else throw new TypeDoesntExistsException();
+
+        if (type.equals("store")) {
+
+            return storeReviewRepository.findAllByReviewer(pageable, member)
+                    .map(storeReview -> GetReviewListResponseDto.toDetailStoreDto(storeReview, type, storeReview.getStore().getId()));
+
+        } else if (type.equals("product")) {
+
+            return productReviewRepository.findAllByReviewer(pageable, member)
+                    .map(productReview -> GetReviewListResponseDto.toDetailProductDto(productReview, type, productReview.getProduct().getId()));
+        }
+
+        throw new TypeDoesntExistsException();
     }
 
 
     /* 검색 OR getAllSimpleReviewInfos */
-    @Transactional(readOnly=true)
+    @Transactional(readOnly = true)
     public Page<GetReviewListResponseDto> searchSimpleReviewInfos(String keyword, String type, Pageable pageable) {
-        if(!StringUtils.hasText(keyword)) { return getAllSimpleReviewInfos(type, pageable);    }
-        if(type.equals("store")) {
-            Page<StoreReview> pages =storeReviewRepository.findAllByContentContainingIgnoreCase(keyword,pageable);
-            return pages.map(GetReviewListResponseDto::toStoreReviewDTO);
-        } else if(type.equals("product")) {
-            Page<ProductReview> pages =productReviewRepository.findAllByContentContainingIgnoreCase(keyword,pageable);
-            return pages.map(GetReviewListResponseDto::toProductReviewDTO);
-        } else throw new TypeDoesntExistsException();
+
+        if (!StringUtils.hasText(keyword))
+            return getAllSimpleReviewInfos(type, pageable);
+
+        if (type.equals("store")) {
+
+            return storeReviewRepository.findAllByContentContainingIgnoreCase(keyword, pageable)
+                    .map(storeReview -> GetReviewListResponseDto.toDetailStoreDto(storeReview, type, storeReview.getStore().getId()));
+
+        } else if (type.equals("product")) {
+
+            return productReviewRepository.findAllByContentContainingIgnoreCase(keyword, pageable)
+                    .map(productReview -> GetReviewListResponseDto.toDetailProductDto(productReview, type, productReview.getProduct().getId()));
+        }
+
+        throw new TypeDoesntExistsException();
     }
 
     /* 스토어 OR 제품 전체 review list 불러오기 */
     private Page<GetReviewListResponseDto> getAllSimpleReviewInfos(String type, Pageable pageable) {
-        if(type.equals("store")) {
-            Page<StoreReview> pages = storeReviewRepository.findAll(pageable);
-            return pages.map(GetReviewListResponseDto::toStoreReviewDTO);
-        } else if(type.equals("product")) {
-            Page<ProductReview> pages = productReviewRepository.findAll(pageable);
-            return pages.map(GetReviewListResponseDto::toProductReviewDTO);
-        } else
-            throw new TypeDoesntExistsException();
+
+        if (type.equals("store")) {
+
+            return storeReviewRepository.findAll(pageable)
+                    .map(storeReview -> GetReviewListResponseDto.toDetailStoreDto(storeReview, type, storeReview.getStore().getId()));
+
+        } else if (type.equals("product")) {
+
+            return productReviewRepository.findAll(pageable)
+                    .map(productReview -> GetReviewListResponseDto.toDetailProductDto(productReview, type, productReview.getProduct().getId()));
+
+        }
+
+        throw new TypeDoesntExistsException();
     }
 
     /* 스토어&제품 ID로 review list 불러오기 */
     @Transactional(readOnly = true)
-    public Page<GetReviewListResponseDto> getSimpleReviewInfos(String type,Long id,Pageable pageable) {
-        if(type.equals("store")) {
+    public Page<GetReviewListResponseDto> getSimpleReviewInfos(String type, Long id, Pageable pageable) {
+
+        if (type.equals("store")) {
+
             Store store = storeRepository.findById(id).orElseThrow(StoreNotFound::new);
             Page<StoreReview> pages = storeReviewRepository.findStoreReviewsByStore(pageable, store);
-            return pages.map(GetReviewListResponseDto::toStoreReviewDTO);
-        } else if(type.equals("product")) {
+            return pages.map(GetReviewListResponseDto::from);
+
+        } else if (type.equals("product")) {
+
             Product product = productRepository.findById(id).orElseThrow(ProductNotFound::new);
             Page<ProductReview> pages = productReviewRepository.findProductReviewsByProduct(pageable, product);
-            return pages.map(GetReviewListResponseDto::toProductReviewDTO);
-        } else throw new TypeDoesntExistsException();
+            return pages.map(GetReviewListResponseDto::from);
+
+        } else
+            throw new TypeDoesntExistsException();
     }
 
 
@@ -124,66 +148,77 @@ public class ReviewService {
         StoreReview storeReview = storeReviewRepository.findById(id).orElseThrow(ReviewNotFound::new);
         //이메일찾기 - 현재멤버 비교 -> dto에 일치여부 포함
         List<String> urls = getStoreReviewImgUrls(storeReview);
-        return buildReviewInfoResponseDto(storeReview.getReviewer().getEmail(),storeReview.getCreatedAt(),storeReview.getStar(),
-                storeReview.getContent(),urls,false);
+        return buildReviewInfoResponseDto(storeReview.getReviewer().getEmail(), storeReview.getCreatedAt(), storeReview.getStar(),
+                storeReview.getContent(), urls, false);
     }
+
     @Transactional(readOnly = true)
     public GetReviewInfoResponseDto getProductReviewInfo(Long id) {
         ProductReview productReview = productReviewRepository.findById(id).orElseThrow(ReviewNotFound::new);
 
         List<String> urls = getProductReviewImgUrls(productReview);
-        return buildReviewInfoResponseDto(productReview.getReviewer().getEmail(),productReview.getCreatedAt(),productReview.getStar(),
-                productReview.getContent(),urls,false);
+        return buildReviewInfoResponseDto(productReview.getReviewer().getEmail(), productReview.getCreatedAt(), productReview.getStar(),
+                productReview.getContent(), urls, false);
     }
 
     /* 상세리뷰 불러오기 : 인증O */
     @Transactional(readOnly = true)
-    public GetReviewInfoResponseDto getStoreReviewInfoWithAuth(Long id,Member member) {
+    public GetReviewInfoResponseDto getStoreReviewInfoWithAuth(Long id, Member member) {
         StoreReview storeReview = storeReviewRepository.findById(id).orElseThrow(ReviewNotFound::new);
         //이메일찾기 - 현재멤버 비교 -> dto에 일치여부 포함
         boolean isWriter = storeReview.getReviewer().getEmail().equals(member.getEmail());
         List<String> urls = getStoreReviewImgUrls(storeReview);
         return buildReviewInfoResponseDto
-                (storeReview.getReviewer().getEmail(),storeReview.getCreatedAt(),storeReview.getStar(),
-                        storeReview.getContent(),urls,isWriter);
+                (storeReview.getReviewer().getEmail(), storeReview.getCreatedAt(), storeReview.getStar(),
+                        storeReview.getContent(), urls, isWriter);
     }
+
     @Transactional(readOnly = true)
-    public GetReviewInfoResponseDto getProductReviewInfoWithAuth(Long id,Member member) {
+    public GetReviewInfoResponseDto getProductReviewInfoWithAuth(Long id, Member member) {
         ProductReview productReview = productReviewRepository.findById(id).orElseThrow(ReviewNotFound::new);
         boolean isWriter = productReview.getReviewer().getEmail().equals(member.getEmail());
 
         List<String> urls = getProductReviewImgUrls(productReview);
         return buildReviewInfoResponseDto
-                (productReview.getReviewer().getEmail(),productReview.getCreatedAt(),productReview.getStar(),
-                        productReview.getContent(),urls,isWriter);
+                (productReview.getReviewer().getEmail(), productReview.getCreatedAt(), productReview.getStar(),
+                        productReview.getContent(), urls, isWriter);
     }
 
     /* 리뷰 삭제 */
     @Transactional
-    public void deleteStoreReview(Long reviewId,Member currentMember) {  // TODO 다른 사용자의 리뷰 삭제를 방지하기 위해 현재 사용자와 리뷰 작성자 비교 : 완료
+    public void deleteStoreReview(Long reviewId, Member currentMember) {  // TODO 다른 사용자의 리뷰 삭제를 방지하기 위해 현재 사용자와 리뷰 작성자 비교 : 완료
         // 리뷰 존재 & 작성자 본인여부 검증
         StoreReview storeReview = storeReviewRepository.findById(reviewId).orElseThrow(ReviewNotFound::new);
-        if(!storeReview.getReviewer().getEmail().equals(currentMember.getEmail())) {throw new MemberNotEqualsException();}
+        if (!storeReview.getReviewer().getEmail().equals(currentMember.getEmail())) {
+            throw new MemberNotEqualsException();
+        }
 
         //image 삭제 : s3에서 삭제 -> StoreReviewImage 삭제
-        List<StoreReviewImage> reviewImages=storeReviewImageRepository.findByStoreReviewId(reviewId);
-        if(reviewImages!=null) {
-            for(StoreReviewImage img:reviewImages) {s3Service.deleteFile(img.getImageUrl());}
+        List<StoreReviewImage> reviewImages = storeReviewImageRepository.findByStoreReviewId(reviewId);
+        if (reviewImages != null) {
+            for (StoreReviewImage img : reviewImages) {
+                s3Service.deleteFile(img.getImageUrl());
+            }
             storeReviewImageRepository.deleteAll(reviewImages);
         }
         //review 삭제 : StoreReview 삭제
         storeReviewRepository.deleteById(reviewId);
     }
+
     @Transactional
-    public void deleteProductReview(Long reviewId,Member currentMember) {
+    public void deleteProductReview(Long reviewId, Member currentMember) {
         // 리뷰 존재 & 작성자 본인여부 검증
         ProductReview productReview = productReviewRepository.findById(reviewId).orElseThrow(ReviewNotFound::new);
-        if(!productReview.getReviewer().getId().equals(currentMember.getId())) {throw new MemberNotEqualsException();}
+        if (!productReview.getReviewer().getId().equals(currentMember.getId())) {
+            throw new MemberNotEqualsException();
+        }
 
         //image 삭제 : s3에서 삭제 -> ProductReviewImage 삭제
-        List<ProductReviewImage> reviewImages=productReviewImageRepository.findByProductReviewId(reviewId);
-        if(reviewImages!=null) {
-            for(ProductReviewImage img:reviewImages) {s3Service.deleteFile(img.getImageUrl());}
+        List<ProductReviewImage> reviewImages = productReviewImageRepository.findByProductReviewId(reviewId);
+        if (reviewImages != null) {
+            for (ProductReviewImage img : reviewImages) {
+                s3Service.deleteFile(img.getImageUrl());
+            }
             productReviewImageRepository.deleteAll(reviewImages);
         }
         //review 삭제 : ProductReview 삭제
@@ -191,7 +226,7 @@ public class ReviewService {
     }
 
 
-    public GetReviewInfoResponseDto buildReviewInfoResponseDto(String email, String createdAt, Integer star, String content, List<String> urls,boolean isWriter) {
+    public GetReviewInfoResponseDto buildReviewInfoResponseDto(String email, String createdAt, Integer star, String content, List<String> urls, boolean isWriter) {
         return GetReviewInfoResponseDto.builder()
                 .writerEmail(email)
                 .createdAt(createdAt)
@@ -204,18 +239,19 @@ public class ReviewService {
 
 
     @Transactional
-    public void uploadFiles(List<MultipartFile> multipartFiles,StoreReview storeReview) {
-        for(MultipartFile file:multipartFiles) {
+    public void uploadFiles(List<MultipartFile> multipartFiles, StoreReview storeReview) {
+        for (MultipartFile file : multipartFiles) {
             StoreReviewImage storeReviewImage = new StoreReviewImage().
-                    getEntity(storeReview,s3Service.uploadFile(file));
+                    getEntity(storeReview, s3Service.uploadFile(file));
             storeReview.getStoreReviewImages().add(storeReviewImage);
         }
     }
+
     @Transactional
-    public void uploadFiles(List<MultipartFile> multipartFiles,ProductReview productReview) {
-        for(MultipartFile file:multipartFiles) {
+    public void uploadFiles(List<MultipartFile> multipartFiles, ProductReview productReview) {
+        for (MultipartFile file : multipartFiles) {
             ProductReviewImage productReviewImage = new ProductReviewImage().
-                    getEntity(productReview,s3Service.uploadFile(file));
+                    getEntity(productReview, s3Service.uploadFile(file));
             productReview.getProductReviewImages().add(productReviewImage);
         }
     }
@@ -225,18 +261,19 @@ public class ReviewService {
     private List<String> getProductReviewImgUrls(ProductReview productReview) {
         List<String> urls = new ArrayList<>();
         List<ProductReviewImage> productReviewImages = productReview.getProductReviewImages();
-        if(productReviewImages!=null) {
-            for(ProductReviewImage image : productReview.getProductReviewImages()) {
+        if (productReviewImages != null) {
+            for (ProductReviewImage image : productReview.getProductReviewImages()) {
                 urls.add(image.getImageUrl());
             }
         }
         return urls;
     }
+
     @NotNull
     private List<String> getStoreReviewImgUrls(StoreReview storeReview) {
         List<String> urls = new ArrayList<>();
         List<StoreReviewImage> storeReviewImages = storeReview.getStoreReviewImages();
-        if(storeReviewImages!=null) {
+        if (storeReviewImages != null) {
             for (StoreReviewImage image : storeReview.getStoreReviewImages()) {
                 urls.add(image.getImageUrl());
             }
